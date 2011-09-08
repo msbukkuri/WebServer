5162b5aa27b4adc849e0f8aa4367043515827ff4
Maanas S. Bukkuri <maanasbukkuri89@gmail.com>
2011-09-07 22:04:34 -0700

    test commit

---
+++ MyFirstWebServer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MyFirstWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new Server();
+            server.ListenForClients();
+            //TcpClient client = new TcpClient();
+
+            //IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3000);
+
+            //client.Connect(serverEndPoint);
+
+            //NetworkStream clientStream = client.GetStream();
+
+            //ASCIIEncoding encoder = new ASCIIEncoding();
+            //byte[] buffer = encoder.GetBytes("Hello Server!");
+
+            //clientStream.Write(buffer, 0, buffer.Length);
+            //clientStream.Flush();
+
+            //byte[] outBuffer = new byte[client.ReceiveBufferSize];
+            //clientStream.Read(outBuffer, 0, buffer.Length);
+            //Console.WriteLine("Received from Server: " + encoder.GetString(outBuffer));
+            
+            Console.ReadLine();
+        }
+
+
+        
+
+    }
+}
---
+++ MyFirstWebServer/Server.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MyFirstWebServer
+{
+    public class Server
+    {
+        private TcpListener tcpListener;
+        private Thread listenThread;
+
+        public Server()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Any, 3000);
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        public void ListenForClients()
+        {
+            this.tcpListener.Start();
+
+            while (true)
+            {
+                //blocks until a client has connected to the server
+                TcpClient client = this.tcpListener.AcceptTcpClient();
+
+                //create a thread to handle communication 
+                //with connected client
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            byte[] message = new byte[tcpClient.ReceiveBufferSize];
+            int bytesRead;
+            string messageString = string.Empty;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, message.Length);
+                    //if(bytesRead > 0)
+                    //    break;
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+            }
+            
+            //Console.WriteLine("Received from Client: " + encoder.GetString(message));
+            
+            byte[] buffer = encoder.GetBytes("HTTP 1.1 200 OK");
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+
+            tcpClient.Close();
+        }
+    }
+}
